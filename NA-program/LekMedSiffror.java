395c17a393190ca50abeca9558a0c2090723deb0
n00nce <49030837+n00nce@users.noreply.github.com>
2019-05-22 19:40:39 +0200

    Spaghetti

--- NA-program/LekMedSiffror.java
+++ NA-program/LekMedSiffror.java
@@ -8,10 +8,10 @@ public class LekMedSiffror {
 // Deklarationer
 //---------------------------------------------------------------------------------------------
 	String 	indata, tal;
-	int fran, till, digRot, antal, n;
-	int multRot = 1;
-	char karaktar;
-	boolean run;
+	int fran, till, digRot, antal, n, siffror, length, multRot;
+	//int multRot = 1;
+	char kar[];
+	boolean run = true;
 //---------------------------------------------------------------------------------------------
 // Konstruktor
 //---------------------------------------------------------------------------------------------
@@ -20,10 +20,13 @@ public class LekMedSiffror {
 		bearbeta();
 	}//end konstraktor
 //---------------------------------------------------------------------------------------------
-// Läser in 3 första tal
-// Räknar ut differens (konstant) = diff
-// Räknar ut ökningen i differens = inc
-// Skriver ut sista 7 talen (4-10)
+// Läser in spannet av tal
+// Kör for-loop med start i talet "fran" för varje tal i spannet
+// multRot sätts till 1 för enkel multiplicering
+// Kollar om multRot>=10 och därför om loopen behöver köras igen eller om loopen är färdig
+// Annars kör loopen, tar ut karaktär för karaktär och multiplicerar med multRot
+// Räknar ut digital rot med mod 9 (%-tecken)
+// Jämför om lika, isåfall lägger till antal
 //---------------------------------------------------------------------------------------------
 	public void bearbeta(){
 		
@@ -32,27 +35,40 @@ public class LekMedSiffror {
 		indata 	= JOptionPane.showInputDialog("Till tal?");
 		till = Integer.parseInt(indata);
 		
-		
+		// RÄKNAR UT MULTIPLIKATIV DIGITAL ROT
         for (int n=fran; n<=till; n=n+1) {
-        	int length = String.valueOf(n).length();
-        	tal = Integer.toString(n);
-        	digRot = (1 + (n - 1) % 9);
-        	//System.out.println(digRot);
-        	while (run=true) {
-	        	for (int i=0; i<length; i=i+1) {
-	        		karaktar = tal.charAt(i);
-	        		multRot = multRot * Character.getNumericValue(karaktar);
-	        		
-	        	}//end for
-	        	
-        	}
-        	System.out.println("Tal "+n+": Digitalrot "+digRot+" Multirot; "+multRot+"");
+        	siffror 	= n;
+        	tal 		= Integer.toString(siffror);
+        	run 		= true;
         	
+        	multRot = 1;
+        	length 	= tal.length();
+        	
+        	for (int i=0; i<=length; i=i+1) {	
+		        if(i == length && multRot >= 10) {
+		        	i		= 0;
+		        	siffror = multRot;
+		        	multRot = 1;
+		        	tal 	= Integer.toString(siffror);
+		        	length 	= tal.length();
+		        }//end if
+		        else if(i 	== length) {
+		        	break;
+		        }//end else if
+	        		
+		        kar 	= new char[10];
+		        kar[i] 	= tal.charAt(i);
+		        
+		        multRot = multRot * Character.getNumericValue(kar[i]);
+        	}//end for
+
+        	digRot = (1 + (n - 1) % 9);
+         
         	if(digRot == multRot) {
         		antal = antal+1;
-        	}
-        	multRot = 1;
-        }
+        	}//end if
+
+        }//end for n
         System.out.println("Antal samma: "+antal+"");
 	}//end bearbeta()
 //---------------------------------------------------------------------------------------------
